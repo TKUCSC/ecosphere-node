e7ef4cee677e15c515561f9a777a967da15d2d46
Elias Mawa <elias@emawa.io>
2020-02-06 17:01:41 +0000

    fix bme680 script

--- read-bme680.py
+++ read-bme680.py
@@ -2,28 +2,29 @@ import bme680
 
 # configuration settings
 try:
-	sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+    try:
+        sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+    except IOError:
+        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 except:
-	print("err")
-	exit(1)
+    print("err: config error")
+    exit(1)
 
 sensor.set_humidity_oversample(bme680.OS_2X)
 sensor.set_pressure_oversample(bme680.OS_4X)
 sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
 
 # read data from sensor
 try:
-	if false == sensor.get_sensor_data():
-		print("err")
-		exit(1)
+    if sensor.get_sensor_data():
+        humidity  = sensor.data.humidity
+        pressure  = sensor.data.pressure
+        ambient_temperature = sensor.data.temperature
+        print(humidity, pressure, ambient_temperature, sep="-")
+
 except:
-	print("err")
-	exit(1)
+    print("err: read error")
+    exit(1)
 
-# store and print data
-humidity  = sensor.data.humidity
-pressure  = sensor.data.pressure
-ambient_temperature = sensor.data.temperature
-
-print(humidity, pressure, ambient_temperature, sep="-")
 exit(0)
