a3b84597535db60562904a41f192c1cc06de0a61
Elias Mawa <elias@emawa.io>
2020-02-06 16:46:57 +0000

    fix bme280 script

--- read-bme280.py
+++ read-bme280.py
@@ -3,26 +3,26 @@ import smbus2
 
 # configuration settings
 port = 1
-bme280_address = 0x77
+address = 0x77
 
 try:
-	bus = smbus2.SMBus(port)
-	calibration_params = bme280.load_calibration_params(bus,address)
+    bus = smbus2.SMBus(port)
+    calibration_params = bme280.load_calibration_params(bus,address)
 except:
-	print("err")
-	exit(1)
+    print("err: config error")
+    exit(1)
 
 # read data from sensor
 try:
-	bme280_data = bme280.sample(bus,address, calibration_params)
+    bme280_data = bme280.sample(bus,address, calibration_params)
 except:
-	print("err")
-	exit(1)
+    print("err: read error")
+    exit(1)
 
 # store and print data
 humidity  = bme280_data.humidity
 pressure  = bme280_data.pressure
 ambient_temperature = bme280_data.temperature
 
-print(humidity, pressure, ambient_temperature, sep="-")
+print(humidity, pressure, ambient_temperature)
 exit(0)
